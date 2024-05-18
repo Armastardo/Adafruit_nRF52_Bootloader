bba3503d0b21c909f5cee0855227bd55953e4bab
Brandon <brandon.abr@hotmail.com>
2024-05-17 23:00:39 -0600

    Alpha bootloader version

--- src/boards/q_stamp_e73/board.h
+++ src/boards/q_stamp_e73/board.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2022 Brandon Beltran
+ * Copyright (c) 2024 Brandon Beltran
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,8 +22,8 @@
  * THE SOFTWARE.
  */
 
-#ifndef Q_STAMP_H
-#define Q_STAMP_H
+#ifndef Q_STAMP_E73_H
+#define Q_STAMP_E73_H
 
 #define _PINNUM(port, pin)    ((port)*32 + (pin))
 
@@ -33,7 +33,7 @@
 /* LED
  *------------------------------------------------------------------*/
 #define LEDS_NUMBER         1
-#define LED_PRIMARY_PIN     _PINNUM(1, 06)   // not yet in board, nor connected
+#define LED_PRIMARY_PIN     _PINNUM(1, 6)   // not yet in board nor connected
 #define LED_STATE_ON        0
 
 /*------------------------------------------------------------------*/
@@ -41,25 +41,25 @@
  *------------------------------------------------------------------*/
 #define BUTTONS_NUMBER    2
 #define BUTTON_1          _PINNUM(0, 18)  // RESET
-#define BUTTON_2          _PINNUM(1, 04)  // unconnected
+#define BUTTON_2          _PINNUM(1, 4)  // unconnected
 #define BUTTON_PULL       NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
-#define BLEDIS_MANUFACTURER  "Quark"
-#define BLEDIS_MODEL         "Q-Stamp (Alpha)"
+#define BLEDIS_MANUFACTURER  "quark"
+#define BLEDIS_MODEL         "q stamp e73 alpha"
 
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
 #define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x5A5B
-#define USB_DESC_CDC_ONLY_PID  0x5A5B
+#define USB_DESC_UF2_PID       0x0029
+#define USB_DESC_CDC_ONLY_PID  0x0029
 
-#define UF2_PRODUCT_NAME  "q_stamp_alpha"
+#define UF2_PRODUCT_NAME  "q_stamp_e73_alpha"
 #define UF2_VOLUME_LABEL  "Q_STAMP"
-#define UF2_BOARD_ID      "q_stamp_alpha"
+#define UF2_BOARD_ID      "q_stamp_e73_alpha"
 #define UF2_INDEX_URL     "https://social.mpwr.xyz"
 
-#endif // Q_STAMP_H
+#endif // Q_STAMP_E73_H
--- src/boards/q_stamp_e73/board.mk
+++ src/boards/q_stamp_e73/board.mk
@@ -1,2 +1,1 @@
-# nrf52 is nrf52832
 MCU_SUB_VARIANT = nrf52840
